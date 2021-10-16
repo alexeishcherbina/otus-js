687853d4bdf871d5f05e064e928281a97a0e77d2
Alexei Shcherbina <alexei.scherbina@gmail.com>
2021-10-16 17:25:32 +0300

    Сделали тест зелёным

--- task3-lesson5/src/getPath.js
+++ task3-lesson5/src/getPath.js
@@ -10,12 +10,16 @@ function getPath(el) {
 
     let selector = '';
     let curEl = el;
-    while (curEl.nodeName.toLowerCase() !== 'body' ) {
+    while (!curEl.id && curEl.nodeName.toLowerCase() !== 'body' ) {
         selector = '>' + getChildNumber(curEl.parentNode, curEl) + selector;
         curEl = curEl.parentNode;
     }
+
+    if (curEl.id) {
+        return '#' + curEl.id + selector;
+    }
     
-    selector = 'BODY ' + selector;
+    selector = 'BODY' + selector;
     return selector;
 }
 
