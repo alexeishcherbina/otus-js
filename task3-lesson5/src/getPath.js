655e1b16b737322df22ab325931a00571f3a6238
Alexei Shcherbina <alexei.scherbina@gmail.com>
2021-10-16 17:18:39 +0300

    Поправляем getPath. Оба теста позеленели

--- task3-lesson5/src/getPath.js
+++ task3-lesson5/src/getPath.js
@@ -1,5 +1,22 @@
 function getPath(el) {
-    return 'section';
+    function getChildNumber(parent, element) {
+        const children = parent.children;
+        if (children.length === 0) {
+            return element.nodeName;
+        } else {
+            return element.nodeName + ':nth-child(' + (Array.prototype.indexOf.call(children, element) + 1) + ')';
+        }
+    }
+
+    let selector = '';
+    let curEl = el;
+    while (curEl.nodeName.toLowerCase() !== 'body' ) {
+        selector = '>' + getChildNumber(curEl.parentNode, curEl) + selector;
+        curEl = curEl.parentNode;
+    }
+
+    selector = 'BODY ' + selector;
+    return selector;
 }
 
 module.exports = getPath;
